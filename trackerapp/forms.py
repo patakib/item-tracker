cdb8b6554a079baa036b1939bf27c34e50bb35f3
patakib <patakibalintka@gmail.com>
2020-11-03 22:51:42 +0100

    updating forms

--- trackerapp/forms.py
+++ trackerapp/forms.py
@@ -3,8 +3,17 @@ from django.forms import ModelForm
 
 from .models import *
 
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
+
 class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
         fields = '__all__'
+        widgets = {
+            'starting_date': DateTimeInput(),
+            'deadline': DateTimeInput(),
+            'action_started': DateTimeInput(),
+            'completion_date': DateTimeInput(),
+        }
