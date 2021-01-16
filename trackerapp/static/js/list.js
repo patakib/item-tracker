ec1428b83cb9160280a544ec4848d56dd2033e8e
patakib <patakibalintka@gmail.com>
2021-01-16 18:14:16 +0100

    creating filter and ordering on the table

--- trackerapp/static/js/list.js
+++ trackerapp/static/js/list.js
@@ -1,11 +1,130 @@
+function filterId() {
+
+  let idSearch = document.getElementById("idSearch");
+  let filter = idSearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterProcess() {
+
+  let processSearch = document.getElementById("processSearch");
+  let filter = processSearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterDay() {
+
+  let daySearch = document.getElementById("daySearch");
+  let filter = daySearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterAgeing() {
+
+  let ageingSearch = document.getElementById("ageingSearch");
+  let filter = ageingSearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterStatus() {
+
+  let statusSearch = document.getElementById("statusSearch");
+  let filter = statusSearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[6];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterName() {
+
+  let nameSearch = document.getElementById("nameSearch");
+  let filter = nameSearch.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[7];
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 function filterSupplier() {
-  // Declare variables
+
   let supplierSearch = document.getElementById("supplierSearch");
   let filter = supplierSearch.value.toUpperCase();
   let table = document.getElementById("myTable");
   let tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     let td = tr[i].getElementsByTagName("td")[8];
     if (td) {
@@ -14,6 +133,64 @@ function filterSupplier() {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+// table sorting
+
+function sortTable(n) {
+  let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 2; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
       }
     }
   }
